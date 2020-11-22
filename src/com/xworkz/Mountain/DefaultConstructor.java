fac442edda2bc2dcbf58d63d3fb12b43ece9f158
darshankpoojary <darshankpoojari@gmail.com>
2020-11-22 20:00:26 +0530

    adding some other classes

--- src/com/xworkz/Mountain/DefaultConstructor.java
+++ src/com/xworkz/Mountain/DefaultConstructor.java
@@ -1,5 +1,6 @@
 package com.xworkz.Mountain;
 
+import java.lang.reflect.Method;
 import java.sql.Time;
 import java.util.Arrays;
 import java.util.Collections;
@@ -17,14 +18,14 @@ import javafx.scene.transform.Scale;
 import sun.util.resources.CalendarData;
 
 public class DefaultConstructor {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Starting the spring frameWork");
-		
+
 		String configFileName = "resources/classes.xml";
-		
+
 		ApplicationContext spring = new ClassPathXmlApplicationContext(configFileName);
-		
+
 		String string = spring.getBean(String.class);
 		Exception exc = spring.getBean(Exception.class);
 		Thread tread = spring.getBean(Thread.class);
@@ -36,7 +37,66 @@ public class DefaultConstructor {
 		Date date = spring.getBean(Date.class);
 		Phaser phaser = spring.getBean(Phaser.class);
 		
+		/*Method[] m = i.getClass().getMethods();
+		System.out.println(m);
+		for (Method method : m) {
+			System.out.println(method.getName());
+		}*/
+		/*Array a=spring.getBean(Array.class);
+		Method [] md=a.getClass().getMethods();
+		for (Method mt : md) {
+			System.out.println(mt.getName());
+		}*/
+		
+		/*Error er=spring.getBean(Error.class);
+		Method [] md=er.getClass().getMethods();
+		for (Method mt : md) {
+			System.out.println(mt.getName());
+		}*/
+		
+		/*Exception ex=spring.getBean(Exception.class);
+		Method [] md=ex.getClass().getMethods();
+		for (Method mt : md) {
+			System.out.println(mt.getName());
+		}*/
+		
+		/*FileHandler fh=spring.getBean(FileHandler.class);
+		Method [] md=fh.getClass().getMethods();
+		for (Method mt : md) {
+			System.out.println(mt.getName());
+		}*/
+		
+		/*Label l=spring.getBean(Label.class);
+		Method [] md=l.getClass().getMethods();
+		for (Method mt : md) {
+			System.out.println(mt.getName());
+		}*/
+		
+		/*List li=spring.getBean(List.class);
+		Method [] md=li.getClass().getMethods();
+		for (Method mt : md) {
+			System.out.println(mt.getName());
+		}*/
+		
+		/*Modifier mf=spring.getBean(Modifier.class);
+		Method [] md=mf.getClass().getMethods();
+		for (Method mt : md) {
+			System.out.println(mt.getName());
+		}*/
+		
+		/*Object o=spring.getBean(Object.class);
+		Method [] md=o.getClass().getMethods();
+		for (Method mt : md) {
+			System.out.println(mt.getName());
+		}*/
+		
+		Stack s=spring.getBean(Stack.class);
+		Method [] md=s.getClass().getMethods();
+		for (Method mt : md) {
+			System.out.println(mt.getName());
+		}
+		
 
-			}
+	}
 
 }
